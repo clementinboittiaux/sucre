a02f2905d0157ac6381c18a3eec026b11fbe33a6
Clementin Boittiaux <Clementin.Boittiaux@ifremer.fr>
2023-10-25 12:19:48 +0200

    sparse version

--- src/loader.py
+++ src/loader.py
@@ -28,83 +28,138 @@ from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
 
 import sfm
-from colmap.scripts.python import read_write_model
 
 
-class Data:
-    def __init__(self):
-        self.data: list[dict[str, Tensor]] = []
-
-    def append(self, u: Tensor, v: Tensor, z: Tensor, Ic: Tensor):
-        self.data.append({'u': u, 'v': v, 'z': z, 'Ic': Ic})
-
-    def to_Ic_z(self, device: str = 'cpu') -> tuple[Tensor, Tensor]:
-        z = torch.full((len(self),), torch.nan, dtype=torch.float32, device=device)
-        Ic = torch.full((len(self),), torch.nan, dtype=torch.float32, device=device)
-        cursor = 0
-        for sample in self.data:
-            length = sample['z'].shape[0]
-            z[cursor: cursor + length] = sample['z'].to(device)
-            Ic[cursor: cursor + length] = sample['Ic'].to(device)
-            cursor += length
-        return Ic, z
-
-    def iter(self) -> tuple[Tensor, Tensor, Tensor, Tensor]:
-        for sample in self.data:
-            yield sample['u'].long(), sample['v'].long(), sample['z'], sample['Ic']
-
-    def iterbatch(self, batch_size: int) -> tuple[Tensor, Tensor, Tensor, Tensor]:
-        for i in range(0, len(self.data), batch_size):
-            yield (
-                torch.hstack([sample['u'] for sample in self.data[i:i + batch_size]]).long(),
-                torch.hstack([sample['v'] for sample in self.data[i:i + batch_size]]).long(),
-                torch.hstack([sample['z'] for sample in self.data[i:i + batch_size]]),
-                torch.hstack([sample['Ic'] for sample in self.data[i:i + batch_size]])
-            )
-
-    def __len__(self):
-        return sum([sample['Ic'].shape[0] for sample in self.data])
+# class Data:
+#     def __init__(self):
+#         self.data: list[dict[str, Tensor]] = []
+#
+#     def append(self, u: Tensor, v: Tensor, z: Tensor, Ic: Tensor):
+#         self.data.append({'u': u, 'v': v, 'z': z, 'Ic': Ic})
+#
+#     def to_Ic_z(self, device: str = 'cpu') -> tuple[Tensor, Tensor]:
+#         z = torch.full((len(self),), torch.nan, dtype=torch.float32, device=device)
+#         Ic = torch.full((len(self),), torch.nan, dtype=torch.float32, device=device)
+#         cursor = 0
+#         for sample in self.data:
+#             length = sample['z'].shape[0]
+#             z[cursor: cursor + length] = sample['z'].to(device)
+#             Ic[cursor: cursor + length] = sample['Ic'].to(device)
+#             cursor += length
+#         return Ic, z
+#
+#     def iter(self) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+#         for sample in self.data:
+#             yield sample['u'].long(), sample['v'].long(), sample['z'], sample['Ic']
+#
+#     def iterbatch(self, batch_size: int) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+#         for i in range(0, len(self.data), batch_size):
+#             yield (
+#                 torch.hstack([sample['u'] for sample in self.data[i:i + batch_size]]).long(),
+#                 torch.hstack([sample['v'] for sample in self.data[i:i + batch_size]]).long(),
+#                 torch.hstack([sample['z'] for sample in self.data[i:i + batch_size]]),
+#                 torch.hstack([sample['Ic'] for sample in self.data[i:i + batch_size]])
+#             )
+#
+#     def __len__(self):
+#         return sum([sample['Ic'].shape[0] for sample in self.data])
 
 
 class MatchesFile:
-    def __init__(self, path: Path, overwrite: bool = False):
+    def __init__(self, path: Path, colmap_model: sfm.COLMAPModel, overwrite: bool = False):
         if overwrite:
             path.unlink(missing_ok=True)
         self.path = path
+        self.colmap_model = colmap_model
 
-    def save_matches(self, matches: sfm.Matches, cP: Tensor):
+    def get_image_list(self) -> list[sfm.Image]:
+        with h5py.File(self.path, 'r', libver='latest') as f:
+            image_list = [self.colmap_model[group_name] for group_name in f]
+        return image_list
+
+    def save_matches(self, matches: sfm.Matches, d: Tensor):
         with h5py.File(self.path, 'a', libver='latest') as f:
             group = f.create_group(matches.image2.name)
             group.create_dataset('u1', data=matches.u1.short().cpu().numpy())
             group.create_dataset('v1', data=matches.v1.short().cpu().numpy())
             group.create_dataset('u2', data=matches.u2.short().cpu().numpy())
             group.create_dataset('v2', data=matches.v2.short().cpu().numpy())
-            group.create_dataset('cP', data=cP.cpu().numpy())
+            group.create_dataset('d', data=d.cpu().numpy())
             group.create_dataset('I', data=np.full((3, len(matches)), np.nan, dtype=np.float32))
 
-    def prepare_matches(self, colmap_model: sfm.COLMAPModel, num_workers: int = 0, device: str = 'cpu'):
-        with h5py.File(self.path, 'r', libver='latest') as f:
-            image_list = [colmap_model[group_name] for group_name in f.keys()]
+    def prepare_matches(self, num_workers: int = 0):
+        image_list = self.get_image_list()
         with h5py.File(self.path, 'r+', libver='latest') as f:
             for image_idx, image_rgb in tqdm.tqdm(
-                    load_image_list(image_list, return_depth=False, num_workers=num_workers)):
+                    load_image_list(image_list, return_depth_map=False, num_workers=num_workers)):
                 image = image_list[image_idx]
                 group = f[image.name]
                 u2 = torch.tensor(group['u2'][()], dtype=torch.int64)
                 v2 = torch.tensor(group['v2'][()], dtype=torch.int64)
                 group['I'][()] = image_rgb[v2, u2].T.numpy()
 
-    def load_channel(self, channel: int, device: str = 'cpu') -> Data:
-        data = Data()
+    def check_integrity(self):
         with h5py.File(self.path, 'r', libver='latest') as f:
             for group in f.values():
-                data.append(
-                    u=torch.tensor(group['u1'][()], device=device),
-                    v=torch.tensor(group['v1'][()], device=device),
-                    z=torch.tensor(group['z'][()], device=device),
-                    Ic=torch.tensor(group['I'][channel], device=device)
-                )
-        return data
+                for dataset_name in ['u1', 'v1', 'u2', 'v2', 'd', 'I']:
+                    dataset = group[dataset_name]
+                    dataset_data = dataset[()]
+                    assert not np.isnan(dataset_data).any(), f'In {self.path}, dataset {dataset.name} contains NaN(s).'
+                    if dataset_name in ['u1', 'v1', 'u2', 'v2', 'I']:
+                        assert np.all(dataset_data >= 0), \
+                            f'In {self.path}, dataset {dataset.name} contains invalid value(s).'
+                    if dataset_name == 'd':
+                        assert np.all(dataset_data > 0), \
+                            f'In {self.path}, dataset {dataset.name} contains null of negative depth(s).'
+
+    def load_matches(self, image: sfm.Image,
+                     device: str = 'cpu') -> tuple[Tensor, Tensor]:
+        size = len(self)
+        i_idx = torch.zeros(size, dtype=torch.int64, device=device)
+        p_idx = torch.zeros(size, dtype=torch.int64, device=device)
+        cP = torch.zeros((3, size), dtype=torch.float32, device=device)
+        I = torch.zeros((3, size), dtype=torch.float32, device=device)
+
+        cursor = 0
+        with h5py.File(self.path, 'r', libver='latest') as f:
+            for i, (group_name, group) in enumerate(f.items()):
+                u1 = torch.tensor(group['u1'][()], device=device, dtype=torch.int64)
+                v1 = torch.tensor(group['v1'][()], device=device, dtype=torch.int64)
+                length = u1.shape[0]
+                i_idx[cursor:cursor+length] = i
+                p_idx[cursor:cursor+length] = v1 * image.camera.width + u1
+                u2 = torch.tensor(group['u2'][()], device=device) + 0.5
+                v2 = torch.tensor(group['v2'][()], device=device) + 0.5
+                d = torch.tensor(group['d'][()], device=device)
+                cP[:, cursor:cursor+length] = image.unproject_depth(u=u2, v=v2, d=d)
+                I[:, cursor:cursor+length] = torch.tensor(group['I'][()], device=device)
+                cursor += length
+
+        cP = torch.sparse_coo_tensor(
+            indices=torch.stack([i_idx, p_idx]),
+            values=cP.T,
+            size=(i, image.camera.height * image.camera.width, 3)
+        ).coalesce()
+        I = torch.sparse_coo_tensor(
+            indices=torch.stack([i_idx, p_idx]),
+            values=I.T,
+            size=(i, image.camera.height * image.camera.width, 3)
+        ).coalesce()
+
+        return cP, I
+
+        # u, v, cP, I = [], [], [], []
+        # with h5py.File(self.path, 'r', libver='latest') as f:
+        #     for group_name, group in f.items():
+        #         image = self.colmap_model[group_name]
+        #         u.append(torch.tensor(group['u1'][()], device=device))
+        #         v.append(torch.tensor(group['v1'][()], device=device))
+        #         u2 = torch.tensor(group['u2'][()], device=device) + 0.5
+        #         v2 = torch.tensor(group['v2'][()], device=device) + 0.5
+        #         d = torch.tensor(group['d'][()], device=device)
+        #         cP.append(image.unproject_depth(u=u2, v=v2, d=d))
+        #         I.append(torch.tensor(group['I'][()], device=device))
+        # return u, v, cP, I
 
     def __len__(self) -> int:
         size = 0
@@ -114,68 +169,56 @@ class MatchesFile:
                     size += group['u1'].shape[0]
         return size
 
+    def __repr__(self) -> str:
+        """String representation"""
+        return f'MatchesFile(path={self.path}, {len(self)} observations)'
+
 
 class ImageDataset(Dataset):
-    def __init__(self, image_list: list[sfm.Image], return_image: bool = True, return_depth: bool = True):
+    def __init__(self, image_list: list[sfm.Image], return_rgb: bool = True, return_depth_map: bool = True):
         self.images = image_list
-        self.return_image = return_image
-        self.return_depth = return_depth
+        self.return_rgb = return_rgb
+        self.return_depth_map = return_depth_map
 
     def __len__(self) -> int:
         return len(self.images)
 
     def __getitem__(self, idx):
         image = self.images[idx]
-        if self.return_image and not self.return_depth:
-            return idx, load_image(image.image_path)
-        elif self.return_depth and not self.return_image:
-            return idx, load_depth(image.depth_path)
-        elif self.return_image and self.return_depth:
-            return idx, load_image(image.image_path), load_depth(image.depth_path)
+        if self.return_rgb and not self.return_depth_map:
+            return idx, load_rgb(image.rgb_path, width=image.camera.width, height=image.camera.height)
+        elif self.return_depth_map and not self.return_rgb:
+            return idx, load_depth_map(image.depth_map_path, width=image.camera.width, height=image.camera.height)
+        elif self.return_rgb and self.return_depth_map:
+            return (
+                idx,
+                load_rgb(image.rgb_path, width=image.camera.width, height=image.camera.height),
+                load_depth_map(image.depth_map_path, width=image.camera.width, height=image.camera.height)
+            )
 
 
-def load_colmap_cameras(model_dir: Path) -> dict[int: read_write_model.Camera]:
-    cameras_bin = model_dir / 'cameras.bin'
-    cameras_txt = model_dir / 'cameras.txt'
-    if cameras_bin.exists():
-        return read_write_model.read_cameras_binary(cameras_bin)
-    elif cameras_txt.exists():
-        return read_write_model.read_cameras_text(cameras_txt)
-    else:
-        raise FileExistsError(f'No cameras file found in {model_dir}.')
+def load_rgb(rgb_path: Path, width: int, height: int) -> Tensor:
+    rgb = cv2.cvtColor(cv2.imread(str(rgb_path)), cv2.COLOR_BGR2RGB) / 255
+    if (rgb.shape[0] != height) or (rgb.shape[1] != width):
+        rgb = cv2.resize(
+            rgb, (width, height),
+            interpolation=cv2.INTER_AREA if width < rgb.shape[1] else cv2.INTER_CUBIC
+        )
+    return torch.tensor(rgb, dtype=torch.float32)
 
 
-def load_colmap_images(model_dir: Path) -> dict[int: read_write_model.Image]:
-    images_bin = model_dir / 'images.bin'
-    images_txt = model_dir / 'images.txt'
-    if images_bin.exists():
-        return read_write_model.read_images_binary(images_bin)
-    elif images_txt.exists():
-        return read_write_model.read_images_text(images_txt)
-    else:
-        raise FileExistsError(f'No cameras file found in {model_dir}.')
-
-
-def load_image(image_path: Path) -> Tensor:
-    return torch.tensor(cv2.cvtColor(cv2.imread(str(image_path)), cv2.COLOR_BGR2RGB)) / 255
-
-
-def load_depth(depth_path: Path) -> Tensor:
-    match depth_path.suffix.lower():
-        case '.png':
-            return torch.tensor(np.int32(cv2.imread(str(depth_path), cv2.IMREAD_UNCHANGED))) / 1000
-        case '.tif' | '.tiff':
-            return torch.tensor(cv2.imread(str(depth_path), cv2.IMREAD_UNCHANGED))
-        case _:
-            raise ValueError(f'Incorrect depth map extension for {depth_path.name}. '
-                             f'Only PNG, TIF and TIFF are supported.')
+def load_depth_map(depth_map_path: Path, width: int, height: int) -> Tensor:
+    depth_map = cv2.imread(str(depth_map_path), cv2.IMREAD_UNCHANGED) / 1000
+    if (depth_map.shape[0] != height) or (depth_map.shape[1] != width):
+        depth_map = cv2.resize(depth_map, (width, height), interpolation=cv2.INTER_NEAREST)
+    return torch.tensor(depth_map, dtype=torch.float32)
 
 
 def load_image_list(
         image_list: list[sfm.Image],
-        return_image: bool = True,
-        return_depth: bool = True,
+        return_rgb: bool = True,
+        return_depth_map: bool = True,
         num_workers: int = 0
 ) -> DataLoader:
-    dataset = ImageDataset(image_list, return_image=return_image, return_depth=return_depth)
+    dataset = ImageDataset(image_list, return_rgb=return_rgb, return_depth_map=return_depth_map)
     return DataLoader(dataset, num_workers=num_workers, collate_fn=lambda x: x[0])
--- src/sfm.py
+++ src/sfm.py
@@ -20,6 +20,7 @@ from __future__ import annotations
 from pathlib import Path
 
 import numpy as np
+import pycolmap
 import torch
 import tqdm
 from PIL import Image as PILImage, ImageDraw
@@ -71,7 +72,6 @@ class Camera:
         self.width = width
         self.height = height
         self.K = K
-        self.K_inv = K.inverse()
 
     def __repr__(self) -> str:
         """String representation"""
@@ -79,63 +79,62 @@ class Camera:
 
 
 class Image:
-    def __init__(self, image_id: int, image_path: Path, depth_path: Path, pose: Pose, camera: Camera):
+    def __init__(self, image_id: int, rgb_path: Path, depth_map_path: Path, pose: Pose, camera: Camera):
         self.id = image_id
-        self.name = image_path.name
-        self.image_path = image_path
-        self.depth_path = depth_path
+        self.name = rgb_path.name
+        self.rgb_path = rgb_path
+        self.depth_map_path = depth_map_path
         self.pose = pose
         self.camera = camera
 
-    def unproject_depth_map(self, depth_map: Tensor, vu: tuple[Tensor, Tensor] = None,
-                            transform: bool = True) -> tuple[Tensor, Tensor, Tensor]:
-        v, u = torch.where(depth_map > 0) if vu is None else vu
+    def unproject_depth(self, u: Tensor, v: Tensor, d: Tensor) -> Tensor:
         uvw = torch.stack([u + 0.5, v + 0.5, torch.ones_like(u)])
-        cp = uvw * depth_map[v, u]
-        cP = self.camera.K_inv.to(cp.device) @ cp
-        if not transform:
-            return u, v, cP
+        cp = uvw * d
+        cP = self.camera.K.to(cp.device).inverse() @ cp
+        return cP
+
+    def depth_map_to_world(self, depth_map: Tensor) -> tuple[Tensor, Tensor, Tensor]:
+        v, u = torch.where(depth_map > 0)
+        cP = self.unproject_depth(u=u, v=v, d=depth_map[v, u])
         wP = self.pose.transform(cP)
         return u, v, wP
 
-    def distance_map(self, depth_map: Tensor) -> Tensor:
-        u, v, cP = self.unproject_depth_map(depth_map, transform=False)
-        distance_map = torch.zeros_like(depth_map)
-        distance_map[v, u] = cP.norm(dim=0)
-        return distance_map
-
-    def project(self, wP: Tensor) -> Tensor:
+    def project_to_view(self, wP: Tensor) -> Tensor:
         cP = self.pose.inverse().transform(wP)
         cp = self.camera.K.to(cP.device) @ cP
         px = cp[:2] / cp[2]
         return px
 
+    def get_rgb(self) -> Tensor:
+        return loader.load_rgb(self.rgb_path, width=self.camera.width, height=self.camera.height)
+
+    def get_depth_map(self) -> Tensor:
+        return loader.load_depth_map(self.depth_map_path, width=self.camera.width, height=self.camera.height)
+
     def match_one_way(self, other: Image, u1: Tensor, v1: Tensor, wP1: Tensor) -> Matches:
-        u2, v2 = other.project(wP1).long()
+        u2, v2 = other.project_to_view(wP1).long()
         args = (0 <= u2) & (u2 < other.camera.width) & (0 <= v2) & (v2 < other.camera.height)
         u1, v1, u2, v2 = u1[args], v1[args], u2[args], v2[args]
         return Matches(image1=self, image2=other, u1=u1, v1=v1, u2=u2, v2=v2)
 
-    def match_two_way(self, other: Image, u1: Tensor, v1: Tensor, wP1: Tensor, u2: Tensor, v2: Tensor,
-                      wP2: Tensor) -> Matches:
+    def match_two_way(self, other: Image, u1: Tensor, v1: Tensor, wP1: Tensor,
+                      u2: Tensor, v2: Tensor, wP2: Tensor) -> Matches:
         matches1 = self.match_one_way(other, u1=u1, v1=v1, wP1=wP1)
         matches2 = other.match_one_way(self, u1=u2, v1=v2, wP1=wP2)
         return matches1 & matches2
 
     def match_images(self, image_list: list[Image], matches_file: loader.MatchesFile, min_cover: float = 0.01,
                      num_workers: int = 0, device: str = 'cpu'):
-        u1, v1, wP1 = self.unproject_depth_map(loader.load_depth(self.depth_path).to(device))
+        u1, v1, wP1 = self.depth_map_to_world(self.get_depth_map().to(device))
         for other_idx, other_depth_map in tqdm.tqdm(
-                loader.load_image_list(image_list, return_image=False, num_workers=num_workers)):
+                loader.load_image_list(image_list, return_rgb=False, num_workers=num_workers)):
             other = image_list[other_idx]
             other_depth_map = other_depth_map.to(device)
-            u2, v2, wP2 = other.unproject_depth_map(other_depth_map)
+            u2, v2, wP2 = other.depth_map_to_world(other_depth_map)
             other_matches = self.match_two_way(other, u1=u1, v1=v1, wP1=wP1, u2=u2, v2=v2, wP2=wP2)
             if len(other_matches) / (self.camera.width * self.camera.height) > min_cover:
-                _, _, cP2 = other.unproject_depth_map(
-                    other_depth_map, vu=(other_matches.v2, other_matches.u2), transform=False
-                )
-                matches_file.save_matches(matches=other_matches, cP=cP2)
+                d2 = other_depth_map[other_matches.v2, other_matches.u2]
+                matches_file.save_matches(matches=other_matches, d=d2)
 
     def __repr__(self) -> str:
         """String representation"""
@@ -152,36 +151,27 @@ class Matches:
         self.v2 = v2
 
     def map(self):
-        match_map = torch.zeros(
-            (self.image1.camera.height, self.image1.camera.width, 2),
-            device=self.u1.device,
-            dtype=self.u1.dtype
-        )
+        match_map = torch.zeros((self.image1.camera.height, self.image1.camera.width, 2),
+                                device=self.u1.device, dtype=self.u1.dtype)
         match_map[self.v1, self.u1, 0] = self.v2
         match_map[self.v1, self.u1, 1] = self.u2
         return match_map
 
     def plot(self, step: int = 10000, color: tuple[float, float, float] = None) -> Image:
-        image1 = loader.load_image(self.image1.image_path)
-        image2 = loader.load_image(self.image2.image_path)
-        imatch = PILImage.fromarray(np.uint8(torch.concat([image1, image2], dim=1) * 255))
+        rgb1 = self.image1.get_rgb()
+        rgb2 = self.image2.get_rgb()
+        imatch = PILImage.fromarray(np.uint8(torch.concat([rgb1, rgb2], dim=1) * 255))
         draw = ImageDraw.Draw(imatch)
         for u1, v1, u2, v2 in zip(self.u1[::step], self.v1[::step], self.u2[::step], self.v2[::step]):
             fill = tuple(np.random.randint(0, 256, 3)) if color is None else color
-            draw.line([(u1, v1), (u2 + image1.shape[1], v2)], fill=fill, width=3)
+            draw.line([(u1, v1), (u2 + rgb1.shape[1], v2)], fill=fill, width=3)
         return imatch
 
     def __and__(self, other: Matches) -> Matches:
         match_map = other.map()
         args = torch.all(match_map[self.v2, self.u2] == torch.stack([self.v1, self.u1]).T, dim=1)
-        return Matches(
-            image1=self.image1,
-            image2=self.image2,
-            u1=self.u1[args],
-            v1=self.v1[args],
-            u2=self.u2[args],
-            v2=self.v2[args]
-        )
+        return Matches(image1=self.image1, image2=self.image2, u1=self.u1[args],
+                       v1=self.v1[args], u2=self.u2[args], v2=self.v2[args])
 
     def __len__(self) -> int:
         """Returns the number of matches"""
@@ -193,14 +183,23 @@ class Matches:
 
 
 class COLMAPModel:
-    def __init__(self, model_dir: Path, image_dir: Path, depth_dir: Path):
+    def __init__(self, model_dir: Path, image_dir: Path, depth_dir: Path, image_scale: float = 1.0):
+        reconstruction = pycolmap.Reconstruction(model_dir)
+
         self.cameras = {}
-        for camera in loader.load_colmap_cameras(model_dir).values():
+        for camera in reconstruction.cameras.values():
+            assert camera.model_name == 'PINHOLE', f'Camera {camera} is not using the PINHOLE model.'
+            width = int(camera.width * image_scale)
+            height = int(camera.height * image_scale)
+            scale_w = width / camera.width
+            scale_h = height / camera.height
             fx, fy, u0, v0 = camera.params
-            self.cameras[camera.id] = Camera(
-                camera_id=camera.id,
-                width=camera.width,
-                height=camera.height,
+            fx, u0 = fx * scale_w, u0 * scale_w
+            fy, v0 = fy * scale_h, v0 * scale_h
+            self.cameras[camera.camera_id] = Camera(
+                camera_id=camera.camera_id,
+                width=width,
+                height=height,
                 K=torch.tensor([
                     [fx, 0, u0],
                     [0, fy, v0],
@@ -209,16 +208,16 @@ class COLMAPModel:
             )
 
         self.images = {}
-        for image in loader.load_colmap_images(model_dir).values():
-            image_path = image_dir / image.name
-            depth_path = (depth_dir / image.name).with_stem('depth_' + image_path.stem).with_suffix('.png')
-            self.images[image.id] = Image(
-                image_id=image.id,
-                image_path=image_path,
-                depth_path=depth_path,
+        for image in reconstruction.images.values():
+            rgb_path = image_dir / image.name
+            depth_map_path = (depth_dir / image.name).with_stem('depth_' + rgb_path.stem).with_suffix('.png')
+            self.images[image.image_id] = Image(
+                image_id=image.image_id,
+                rgb_path=rgb_path,
+                depth_map_path=depth_map_path,
                 pose=Pose(
-                    torch.tensor(image.qvec2rotmat(), dtype=torch.float32),
-                    torch.tensor(image.tvec.reshape(3, 1), dtype=torch.float32)
+                    R=torch.tensor(image.rotmat(), dtype=torch.float32),
+                    t=torch.tensor(image.tvec, dtype=torch.float32).view(3, 1)
                 ).inverse(),
                 camera=self.cameras[image.camera_id]
             )
@@ -232,3 +231,7 @@ class COLMAPModel:
         :return: Image.
         """
         return self.images[self.imagename2id[image_name]]
+
+    def __repr__(self) -> str:
+        """String representation"""
+        return f'COLMAPModel({len(self.images)} images)'
